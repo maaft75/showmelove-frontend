c677fd4e8e41c0fd1900b262a71c11df9df5950c
ayodeji adedeji <ayodejiadedeji8@gmail.com>
2020-12-25 13:36:37 +0100

    homePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+//Router
+import { AppRoutes } from './approuting/approuting.module';
+
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+//Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/approuting/approuting.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+
+export const AppRoutes : Routes = [
+  {path : "home" , component : HomeComponent },
+  {path : "**", redirectTo : "/home", pathMatch : "full"}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule
+  ],
+  exports :[
+    RouterModule
+  ]
+})
+
+export class ApproutingModule { }
