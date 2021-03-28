6ffd5c76ac0e105c0c3d691e8d5b525a2bf26a68
maaft75 <ayodejiadedeji8@gmail.com>
2021-03-28 01:42:46 +0100

    addedGuardsnUpdatedProj3

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -28,7 +28,7 @@ export class DashboardComponent implements OnInit {
     })
 
     //Get Fundraiser created by Logged in User
-    this.fundraiserService.getFundraiser(1).subscribe(
+    this.fundraiserService.getFundraiser(Number(this.Id)).subscribe(
       (data) => 
       {
         this.fundRaisersByLoggedInUser = data;
